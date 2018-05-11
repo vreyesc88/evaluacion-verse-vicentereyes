a303cec34d33327415fe2b2a58f322225e2be986
vreyesc88 <38965881+vreyesc88@users.noreply.github.com>
2018-05-11 16:24:39 -0500

    actulizacion app.js

--- app.js
+++ app.js
@@ -14,15 +14,13 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
 
 // funciones
-app.use(myconnection(mysql,{
-    //host: 'mysql.hostinger.mx', 
+app.use(myconnection(mysql, {    
     host: 'us-cdbr-gcp-east-01.cleardb.net',
     user: 'b1b3df40516a05',
     password: '847b91d5',
     port: 3306,
     database: 'gcp_ff6f1548f5c45a8d8600'
-//}, 'single'));
-}));
+}, 'single'));
 
 // requerir metodos desde formularios
 app.use(express.urlencoded({extended: false}));
@@ -33,6 +31,6 @@ app.use('/', registroRutas);
 //archivo static
 app.use(express.static(path.join(__dirname, 'src/views')));
 
-app.listen(5000, () => {
-    console.log('Server on port 5000');
+app.listen(app.get('port'), () => {
+    console.log('Server on');
 });
